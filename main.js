1078cfc87b1968ed358c13f85d8b4a777259ac15
Emily Deans <emilyldeans@gmail.com>
2017-07-18 15:15:05 -0400

    Added initial innerHTML

--- main.js
+++ main.js
@@ -7,9 +7,36 @@ fetch(baseUrl)
       return;
     }
     response.json().then(function(data){
-      console.log(response);
+      console.log(data);
     });
     let rec_input = document.querySelector(".recipe_input");
     let userReq = baseUrl + "?q=" + rec_input;
     console.log(userReq);
+    let template = `
+    <ul class="upper"></ul>
+    <li>
+    <img src="${data.thumbnail}" alt="image_not_shown">
+    <a href=${data.href}><h4>${data.title}</h></a>
+    </li>
+    <ul class="upper"></ul>
+    `
+    container.innerHTML = template;
   })
+
+
+//
+// <section class="container">
+//
+//   <ul class="top_row">
+//     <li class="upper"></li>
+//     <li class="upper"></li>
+//     <li class="upper"></li>
+//     <li class="upper"></li>
+//   </ul>
+//   <ul class="bottom_row">
+//     <li class="lower"></li>
+//     <li class="lower"></li>
+//     <li class="lower"></li>
+//     <li class="lower"></li>
+//   </ul>
+// </section>`
