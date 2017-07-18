c1a6c91dde9078e721f2ccda17b09ea01390a344
Emily Deans <emilyldeans@gmail.com>
2017-07-18 14:54:32 -0400

    Created fetch and also assigned variables for baseUrl

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-fetch("http://www.recipepuppy.com/api/");
+const baseUrl = "https://crossorigin.me/http://www.recipepuppy.com/api/"
 
+fetch(baseUrl)
   .then(function(response) {
     if (response.status !== 200) {
       console.log(response.status);
       return;
     }
     response.json().then(function(data){
-
+      console.log(response);
     });
+    let rec_input = document.querySelector(".recipe_input");
+    let userReq = baseUrl + "?q=" + rec_input;
+    console.log(userReq);
   })
