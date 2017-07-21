d94e247a42316702a2b1a28a52a79745cf1807e9
Emily Deans <emilyldeans@gmail.com>
2017-07-20 20:23:29 -0400

    Created forEach and styled results

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
-const baseUrl = "https://crossorigin.me/http://www.recipepuppy.com/api/";
+let baseUrl = "https://crossorigin.me/http://www.recipepuppy.com/api/";
 
 const container = document.querySelector(".container");
 let rec_input = document.querySelector(".recipe_input");
+let button = document.querySelector("button");
 
-document.querySelector("button").addEventListener("click", function() {
-  console.log(rec_input.value);
+button.addEventListener("click", function(e) {
+  let userReq = baseUrl + "?q=" + rec_input.value;
+  e.preventDefault();
+  // baseUrl = `${baseUrl}/?q=${search.value}`;
 
-let userReq = baseUrl + "?q=" + rec_input.value;
-
-fetch(userReq)
-  .then(function(response) {
-    if (response.status !== 200) {
-      console.log(response.status);
-      return;
-      }
-      response.json().then(function(data){
-        console.log(data);
-
-            let innerData = data.results[0];
-            let template = `
-            <ul class="upper"></ul>
-              <li>
-                <img src="${innerData.thumbnail}" alt="image_not_shown">
-                <a href=${innerData.href}><h4>${innerData.title}</h></a>
-                <p>${innerData.ingredients}</p>
-              </li>
-            <ul class="upper"></ul>
-            `
-            container.innerHTML = template;
-      });
+  fetch(userReq)
+    .then(function(response) {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+        }
+        response.json().then(function(data){
+          let templateContainer = "";
+          data.results.forEach(function(items) {
+            let imageSrc = "http://via.placeholder.com/350x150"
+            if (items.thumbnail !== "") {
+              imageSrc = items.thumbnail;
+            }
+            console.log(data);
+              let template = `
+              <ul>
+                <li>
+                  <img src="${imageSrc}" alt="image_not_shown">
+                  <h1>${items.title}</h1>
+                  <h3>Ingredients:</h3>
+                  <p>${items.ingredients}</p>
+                  <span><a href="${items.href}">See the Recipe</a></span>
+                </li>
+              </ul>
+              `
+              templateContainer += template;
+            });
+            container.innerHTML = templateContainer;
+            rec_input.value = "";
+        });
     })
   });
 
